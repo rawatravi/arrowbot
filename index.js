b36bd812d832836a32cad0971589f5498b9fbe92
Utkarsh Shukla <utk.shukla@gmail.com>
2019-02-01 16:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 exports.printMsg = function() {
     console.log("This is a message from the demo package");
+    //Set Application Key and Token  ///Token To be Added Later On
+    //Add User
+    //Get User
+    //Set User Variable in Existing User
+    //Get User Variable
+    //Add Dialogflow Logging
   }
